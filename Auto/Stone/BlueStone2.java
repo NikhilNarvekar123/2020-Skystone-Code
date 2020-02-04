e7ae894197582ce33c259ed6590ee91680f7c590
Rust In Peace (FTC 12977) <57912023+RIP-12977@users.noreply.github.com>
2020-02-04 11:16:50 -0600

    Update BlueStone2.java

--- Auto/Stone/BlueStone2.java
+++ Auto/Stone/BlueStone2.java
@@ -1,5 +1,6 @@
-package org.firstinspires.ftc.teamcode.auto;
+package org.firstinspires.ftc.teamcode.Auto;
 
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -17,93 +18,106 @@ import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 
+
 @Autonomous(group="Stone", name="BlueStone2")
 
 public class BlueStone2 extends AutoSystem implements AutoValues {
 
-public BlueStone2()
-{
-  super("VuforiaOn","Blue","WebCam");
-}
-
-public void runOpMode() throws InterruptedException {
-
-  super.runOpMode();
-
-  /* Main Loop */
-  while(opModeIsActive()) {
-
-    strafeRight(15, SIDEWAYS_SPEED_NORM, 1);
-    sleep(DEFAULT_METHOD_DELAY);
-
-    String stoneLocation = scanner();
-    sleep(750);
-
-    telemetry.addLine(stoneLocation);
-    telemetry.update();
-
-    int deltaPos = 0;
-
-    //nikhil cant code...also i love nikhil - from sriman t
-    if(stoneLocation.equals("BLUE_LEFT")) {
-      deltaPos = -7;
-    } else if(stoneLocation.equals("BLUE_CENTER")) {
-      moveBackward(7, LINEAR_SPEED_NORM, 1);
-      deltaPos = 2;
-    } else {
-      moveBackward(14, LINEAR_SPEED_NORM, 1);
-      deltaPos = 4;
-    }
-
-    TFODShutDown();
-
-    strafeRight(20, SIDEWAYS_SPEED_MAX, 1);
-    sleep(DEFAULT_METHOD_DELAY);
-
-    clampBlock();
-    sleep(DEFAULT_SERVO_DELAY);
-
-    strafeLeft(7, SIDEWAYS_SPEED_MAX, 2);
-    sleep(DEFAULT_METHOD_DELAY);
-
-    moveForward(47 + deltaPos, LINEAR_SPEED_MAX, 1);
-    sleep(DEFAULT_METHOD_DELAY);
-
-    releaseBlock();
-    sleep(DEFAULT_SERVO_DELAY);
-
-    moveBackward(12 + deltaPos, LINEAR_SPEED_MAX, 1);
-    sleep(DEFAULT_METHOD_DELAY);
-
-
-    // Second Stone
-    int valTune = 50; //deltaPos???
-    moveBackward(valTune, LINEAR_SPEED_MAX, 1);
-    sleep(DEFAULT_METHOD_DELAY);
-
-    strafeRight(14, SIDEWAYS_SPEED_NORM, 1);
-    sleep(DEFAULT_METHOD_DELAY);
-
-    clampBlock();
-    sleep(DEFAULT_SERVO_DELAY);
-
-    strafeLeft(5, SIDEWAYS_SPEED_MAX, 1);
-    sleep(DEFAULT_METHOD_DELAY);
-
-    valTune = 45; //deltaPos???
-    moveForward(45, LINEAR_SPEED_MAX, 1);
-    sleep(DEFAULT_METHOD_DELAY);
-
-    releaseBlock();
-    sleep(DEFAULT_SERVO_DELAY);
-
-    valTune = 10; //deltaPos??
-    moveBackward(10 + deltaPos, LINEAR_SPEED_MAX, 1);
-    sleep(DEFAULT_SERVO_DELAY);
-
-    break;
-
+  public BlueStone2() {
+    super("VuforiaOn","blue","WebCam");
   }
-}
+
+  public void runOpMode() throws InterruptedException {
+
+    super.runOpMode();
+
+    /* Main Loop */
+    while(opModeIsActive()) {
+
+      // Move Clamp Servo Out Beforehand
+      blockServoClamp.setPosition(0.3);
+
+      // Strafe right to detect
+      strafeRight(12, SIDEWAYS_SPEED_MAX, 1);
+
+      // Allow Time for robot to stabilize and detect skblockServoClampPosstone
+      sleep(DETECTION_DELAY);
+      String stoneLocation = scanner();
+      telemetry.addLine(stoneLocation);
+      telemetry.update();
+
+      // Move to Skystone
+      if(stoneLocation.equals("BLUE_LEFT")) {
+        //
+      } else if(stoneLocation.equals("BLUE_CENTER")) {
+        moveBackward(7, LINEAR_SPEED_MIN, 1);
+      } else {
+        moveBackward(14, LINEAR_SPEED_MIN, 1);
+      }
+      sleep(DEFAULT_METHOD_DELAY);
+
+      // Shutdown Vuforia Engine
+      TFODShutDown();
+
+      // Move to pickup first skystone
+      strafeRight(13, SIDEWAYS_SPEED_MAX, 1);
+      sleep(DEFAULT_METHOD_DELAY);
+
+      // Grab Skystone
+      clampBlock();
+
+      // Strafe Outwards
+      strafeLeft(6, SIDEWAYS_SPEED_MAX, 1);
+
+      // Deposit first skystone
+      if(stoneLocation.equals("BLUE_LEFT"))
+        moveForward(30, LINEAR_SPEED_MAX, 1);
+      else if(stoneLocation.equals("BLUE_CENTER"))
+        moveForward(38, LINEAR_SPEED_MAX, 1);
+      else
+        moveForward(45, LINEAR_SPEED_MAX, 1);
+      sleep(DEFAULT_METHOD_DELAY);
+
+      // Release Skystone
+      releaseBlock();
+      sleep(DEFAULT_SERVO_DELAY);
+
+      // Move to Second Skystone
+      if(stoneLocation.equals("BLUE_LEFT"))
+        moveBackward(50, LINEAR_SPEED_MAX, 1);
+      else if(stoneLocation.equals("BLUE_CENTER"))
+        moveBackward(58, LINEAR_SPEED_MAX, 1);
+      else
+        moveBackward(56, LINEAR_SPEED_MAX, 1);
+      sleep(DEFAULT_METHOD_DELAY);
+
+      // Move to pickup second skystone
+      strafeRight(6, SIDEWAYS_SPEED_MAX, 1);
+      sleep(DEFAULT_METHOD_DELAY);
+
+      // Grab Second Skystone
+      clampBlock();
+
+      // Move Outwards
+      strafeLeft(6, SIDEWAYS_SPEED_MAX, 1);
+
+      // Deposit Second Skystone
+      if(stoneLocation.equals("BLUE_LEFT"))
+        moveForward(55, LINEAR_SPEED_MAX, 1);
+      else if(stoneLocation.equals("BLUE_CENTER"))
+        moveForward(63, LINEAR_SPEED_MAX, 1);
+      else
+        moveForward(71, LINEAR_SPEED_MAX, 1);
+      sleep(DEFAULT_METHOD_DELAY);
+
+      // Release Second Skystone
+      releaseBlock();
+      sleep(DEFAULT_SERVO_DELAY);
+
+      // Move to Park
+      moveBackward(9, LINEAR_SPEED_MAX, 1);
+      break;
+    }
+  }
 
 }
